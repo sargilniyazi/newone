4df756d7129154ece92b2f6de062924c5941a592
sargilniyazi <sargilexam@gmail.com>
2022-10-30 13:12:34 +0530

    done

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
 public static void main(String [] args){
 System.out.println("Hello world");
-System.out.printlm("These are the changes");
+System.out.println("These are the changes");
 }
 }
